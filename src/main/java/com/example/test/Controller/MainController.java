2a49aa8eab7deccf537eeb1f0eabdab4e06daa09
KYUWON1 <127181209+KYUWON1@users.noreply.github.com>
2024-07-22 17:08:49 +0900

    feat:로그아웃및인증번호제한시간추가

--- src/main/java/com/example/test/Controller/MainController.java
+++ src/main/java/com/example/test/Controller/MainController.java
@@ -1,10 +1,14 @@
 package com.example.test.Controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Collection;
@@ -27,4 +31,15 @@ public class MainController {
 
         return "main controller " + name+" : "+role;
     }
+
+    /* 메인페이지 로그아웃 */
+    @GetMapping("/logout.do")
+    public ResponseEntity<?> logoutMainGET(HttpServletRequest request) throws Exception{
+        HttpSession session = request.getSession();
+        session.invalidate();
+
+        return ResponseEntity.ok("로그아웃 성공");
+
+    }
+
 }
