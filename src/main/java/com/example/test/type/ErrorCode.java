f4c68a70bece2938cd7ad78516c48dd7cdc33745
KYUWON1 <127181209+KYUWON1@users.noreply.github.com>
2024-07-24 01:05:22 +0900

    feat:비밀번호업데이트API추가

--- src/main/java/com/example/test/type/ErrorCode.java
+++ src/main/java/com/example/test/type/ErrorCode.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 public enum ErrorCode {
     USER_NOT_FOUND("사용자가 없습니다."),
     VERIFY_CODE_NOT_MATCH("인증코드가 옳바르지 않습니다"),
-    PASSWORD_CHECK_UN_MATCH("설정하실 비밀번호를 한번더 확인해주세요");
+    PASSWORD_CHECK_UN_MATCH("설정하실 비밀번호를 한번 더 확인해주세요"),
+    PASSWORD_UN_MATCH("비밀번호가 잘못되었습니다.");
     private final String description;
 }
