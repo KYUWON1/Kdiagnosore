a98d3808df92e07bc7d9c5920bfdf6ec35f3aedf
skdms <skdms1015@naver.com>
2024-07-28 17:23:33 +0900

    fix : 추가 UI 구현 및 회원정보, 비밀번호 수정

--- frontend/screens/MyPageModScreen.js
+++ frontend/screens/MyPageModScreen.js
@@ -1,14 +1,71 @@
-import React from "react";
-import {View, Text,SafeAreaView, StyleSheet} from 'react-native'
+import React, {useState, useEffect} from "react";
+import {View, Text,SafeAreaView, StyleSheet, TextInput, TouchableOpacity} from 'react-native'
 import AntDesign from 'react-native-vector-icons/AntDesign'
 
 const MyPageModScreen = ({navigation}) => {
+    const [UserName, setUserName] = useState("");
+    const [Email, setEmail] = useState("");
+    const [Password, setPassword] = useState("ooooooooo");
+    const [PhoneNum, setPhoneNum] = useState("");
+    const [ProtectorName, setProtectorName] = useState("");
+    const [ProtectorNum, setProtectorNum] = useState("");
     return(
         <SafeAreaView style={styles.container}>
             <View style={styles.header}>
                 <AntDesign name='left' size={25} style={{marginHorizontal:10,}} onPress={()=>navigation.navigate('Setting1')}/>
-                <Text style={{fontSize:20, fontWeight:700}}>내 정보 수정</Text>
+                <Text style={{fontSize:20, fontWeight:700}}>회원정보 수정</Text>
             </View>
+            <View style={{ marginTop:20, alignItems: 'center' }}>
+                    <Text style={styles.label}>이름</Text>
+                    <TextInput
+                        style={styles.input}
+                        value={UserName}
+                        onChangeText={setUserName}
+                    />
+                    <Text style={styles.label}>이메일</Text>
+                    <TextInput
+                        style={styles.input}
+                        value={Email}
+                        onChangeText={setEmail}
+                    />
+                    <Text style={styles.label}>비밀번호</Text>
+                    <View style={{flexDirection:"row", justifyContent:'space-between', width:'90%'}}>
+                        <TextInput
+                            style={styles.input1}
+                            value={Password}
+                            placeholder='대소문자, 특수문자, 숫자~~'
+                            onChangeText={setPassword}
+                            secureTextEntry
+                        />
+                        <TouchableOpacity style={styles.button1} onPress={{}}>
+                            <Text style={{ fontSize: 20, color: '#fff' }}>수정</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <Text style={styles.label}>전화번호</Text>
+                    <TextInput
+                        style={styles.input}
+                        value={PhoneNum}
+                        onChangeText={setPhoneNum}
+                        keyboardType="number-pad"
+                    />
+                    <Text style={styles.label}>보호자 이름</Text>
+                    <TextInput
+                        style={styles.input}
+                        value={ProtectorName}
+                        onChangeText={setProtectorName}
+                    />
+                    <Text style={styles.label}>보호자 전화번호</Text>
+                    <TextInput
+                        style={styles.input}
+                        value={ProtectorNum}
+                        onChangeText={setProtectorNum}
+                        keyboardType="number-pad"
+                    />
+                    <TouchableOpacity style={styles.button} onPress={{}}>
+                        <Text style={{ fontSize: 20, color: '#fff' }}>정보 변경</Text>
+                    </TouchableOpacity>
+                </View>
+
         </SafeAreaView>
     )
 }
@@ -27,6 +84,50 @@ const styles = StyleSheet.create({
         borderStyle: "solid",
         borderBottomColor:"#E0E0E0",
         overflow: "hidden",
+    },
+    label:{
+        width:'90%',
+        marginLeft:10,
+        fontSize:18,
+    },
+    input:{
+      width:'90%',
+      height:50,
+      borderColor:"#E0E0E0",
+      borderWidth:1,
+      borderRadius:10,
+      backgroundColor:"white",
+      paddingVertical:10,
+      paddingHorizontal:10,
+      marginVertical:10,
+      fontSize:18,
+    },
+    input1:{
+        width:'50%',
+        height:50,
+        backgroundColor:"white",
+        paddingVertical:10,
+        paddingHorizontal:10,
+        marginVertical:10,
+        fontSize:18,
+      },
+    button:{
+        marginVertical:15,
+        width:'90%',
+        height:50,
+        alignItems:'center',
+        justifyContent:'center',
+        backgroundColor:'#000',
+        borderRadius:10,
+    },
+    button1:{
+        marginVertical:15,
+        width:50,
+        height:28,
+        alignItems:'center',
+        justifyContent:'center',
+        backgroundColor:'#000',
+        borderRadius:10,
     },
 
 });
--- frontend/screens/SettingScreen.js
+++ frontend/screens/SettingScreen.js
@@ -1,11 +1,18 @@
-import React from "react";
-import { View, Text, SafeAreaView, StyleSheet, Alert, TouchableOpacity } from 'react-native';
-import AntDesign from 'react-native-vector-icons/AntDesign';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import {React,useState} from "react";
+import {View, Text,SafeAreaView, StyleSheet} from 'react-native'
+import AntDesign from 'react-native-vector-icons/AntDesign'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons"
 import axios from 'axios';
+import Toogle from "../component/Toogle";
 
-const SettingScreen = ({ navigation }) => {
+const SettingScreen = ({navigation}) => {
+    const [isOn, setIsOn] = useState(false);
+    const handleToggle = () => {
+        //알림 설정 버튼 on/off
+      setIsOn(previousState => !previousState);
+    };
+
     const handleLogout = async () => {
         // 토큰을 삭제하고 기본 헤더에서 Authorization 제거
         delete axios.defaults.headers.common['Authorization'];
@@ -13,31 +20,29 @@ const SettingScreen = ({ navigation }) => {
         navigation.navigate('Login'); // 로그인 화면으로 네비게이트
     };
 
-    return (
+    return(
         <SafeAreaView style={styles.container}>
             <View style={styles.header}>
                 <Text style={{ fontSize: 20, fontWeight: '700' }}>설정</Text>
             </View>
-            <View style={styles.setting}>
-                <View style={styles.setting1}>
-                    <MaterialCommunityIcons name="account" size={25} style={{ marginLeft: 15 }} />
-                    <Text style={styles.settingcontent}>내 정보 수정</Text>
+            <View style={styles.settingalarm}>
+                <View style={styles.setting1_box}>
+                    <View style={styles.setting1}>
+                        <MaterialCommunityIcons name='alarm' size={25} style={{marginLeft:15}}/>
+                        <Text style={styles.settingcontent}>알림 설정</Text>
+                    </View>
+                    <Toogle onToggle={handleToggle} isOn={isOn}/>
                 </View>
-                <AntDesign name='right' size={25} style={{ marginHorizontal: 10 }} onPress={() => navigation.navigate('MyPageMod')} />
+                <View>
+                    <Text style={styles.alarmdetail}>알림을 설정하면 챗봇이 주기적으로 대화를 신청합니다.</Text>
+                </View>
             </View>
             <View style={styles.setting}>
                 <View style={styles.setting1}>
-                    <MaterialCommunityIcons name='alarm' size={25} style={{ marginLeft: 15 }} />
-                    <Text style={styles.settingcontent}>알림 설정</Text>
+                    <MaterialCommunityIcons name='account-circle' size={25} style={{marginLeft:15}}/>
+                    <Text style={styles.settingcontent}>회원정보 수정</Text>
                 </View>
-                <AntDesign name='right' size={25} style={{ marginHorizontal: 10 }} onPress={() => navigation.navigate('SetAlarm')} />
-            </View>
-            <View style={styles.setting}>
-                <View style={styles.setting1}>
-                    <MaterialCommunityIcons name='account-circle' size={25} style={{ marginLeft: 15 }} />
-                    <Text style={styles.settingcontent}>계정 설정</Text>
-                </View>
-                <AntDesign name='right' size={25} style={{ marginHorizontal: 10 }} onPress={() => navigation.navigate('SetAccount')} />
+                <AntDesign name='right' size={25} style={{marginHorizontal:10,}} onPress={()=>navigation.navigate('MyPageMod')}/>
             </View>
             <TouchableOpacity style={styles.setting} onPress={handleLogout}>
                 <View style={styles.setting1}>
@@ -76,19 +81,37 @@ const styles = StyleSheet.create({
         borderBottomWidth: 1,
         paddingVertical: 20,
     },
-    setting1: {
-        flexDirection: 'row',
-        alignItems: 'center',
+    settingalarm:{
+        borderBottomColor:'#B6B6B6', 
+        borderBottomWidth:1,
+        paddingVertical:20,
     },
-    settingcontent: {
-        fontSize: 18,
-        marginLeft: 10,
+    setting1_box:{
+        justifyContent:'space-between',
+        flexDirection:'row',
+        paddingRight:20,
     },
-    settingout: {
-        justifyContent: 'center',
-        paddingVertical: 20,
-        paddingHorizontal: 20,
+    setting1:{
+        flexDirection:'row',
+        alignItems:'center',
+    },
+    settingcontent:{
+        fontSize:18,
+        marginLeft:10,
+
+    },
+    alarmdetail:{
+        fontSize:15,
+        marginLeft:50,
+        marginTop:10,
+        color : '#828282',
+    },
+    settingout:{
+        justifyContent:'center',
+        paddingVertical:20,
+        paddingHorizontal:20,
     }
+
 });
 
 export default SettingScreen;
