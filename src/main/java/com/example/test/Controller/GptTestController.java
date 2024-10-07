653605b225b00c00bfb385de8bbd8baca8e75888
syc <yechan19202127@gmail.com>
2024-10-07 15:15:23 +0900

    다이어리 객관식 테스트

--- src/main/java/com/example/test/Controller/GptTestController.java
+++ src/main/java/com/example/test/Controller/GptTestController.java
@@ -182,16 +182,17 @@ public class GptTestController {
         return "Create Test Question!";
     }
 
-    @GetMapping("/createTestFromDiary")
-    public String diaryTest(){
+    @GetMapping("/createTestFromDiary/jugwan")
+    public String createTestJugwanByDiary(){
         List<String> allUserId = userService.findAllUserId();
         for(String userId : allUserId){
             String prompt = """
                 너는 이제부터 일상 정보 기반 질문 생성 AI야.
                 모든 문장의 끝에는 @ 를 항상 붙여주고, 줄 바꿈은 하지 말아줘.
                 아래는 사용자가 하루동안 있었던 일들을 기록한 텍스트야.
-                위의 내용을 바탕으로 질문과 예상 답변과 근거을 생성해줘.
-                질문과 예상 답변 그리고 근거를 생성하는 방법을 차례대로 설명해줄께.
+                위의 내용을 바탕으로 질문과 예상 답변과 근거를 생성해줘.
+                질문과 예상 답변 그리고 근거를 생성하는 방법을 차례대로 설명해줄게.
+                Date와 Time은 해당 채팅을 입력한 날짜와 시간이야.
                 """;
 
             String add = """
@@ -227,6 +228,72 @@ public class GptTestController {
                 데이터 파싱을 위해서 반드시 위에 제공해준 예시처럼 데이터를 전달해줘.
                 근거에도 마지막에 @를 붙여줘.
                 줄바꿈은 하지 말아줘.
+                """;
+            String diaryMessages = diaryService.getYesterdayDiaryData(userId);
+//            String chatMessages = chatService.getChatMessage(userId,
+//                    LocalDate.now().format(DateTimeFormatter.ISO_LOCAL_DATE));
+            if(diaryMessages.equals("No Data")){
+                continue;
+            }
+//            String chatMessages = """
+//                    오늘 아침에는 일어나서 혼자 밥을 먹었다. 조금 외로웠던거 같다. 설거지를 마치고 점심에는 강아지 초코와 함께 산책을 나갔다.
+//                    옆집 할머니가 마침 나와계셔서 수다를 떨다가 들어왔다. 저녁에는 나물을 넣어서 비빔밥을 만들어 먹었다. 고추장을 너무 많이 넣어서 맵더라.
+//                    밤에는 아들내미가 웬일로 먼저 전화를 했다. 목소리 들으니까 좋더라. 곧 추석때 온다고 하니까 명절음식 많이 해놓아야겠다.
+//                    """;
+            prompt += diaryMessages + add;
+            System.out.println("userId: "+ userId);
+            System.out.println(prompt);
+            GPTRequestDTO request = new GPTRequestDTO(model, prompt);
+            GPTResponseDTO response = template.postForObject(apiURL, request, GPTResponseDTO.class);
+            String createdTest = response.getChoices().get(0).getMessage().getContent();
+            System.out.println(createdTest);
+            chatService.saveTestChat(userId,createdTest);
+            log.info("Test Created {}.",userId);
+        }
+
+        log.info("Test Created End.");
+        return "Create Test Question!";
+    }
+
+    @GetMapping("/createTestFromDiary/gaggwan")
+    public String createTestGaggwanByDiary(){
+        List<String> allUserId = userService.findAllUserId();
+        for(String userId : allUserId){
+            String prompt = """
+                너는 이제부터 일상 정보 기반 질문 생성 AI야.
+                모든 문장의 끝에는 @ 를 항상 붙여주고, 줄 바꿈은 하지 말아줘.
+                아래는 사용자가 하루동안 있었던 일들을 기록한 텍스트야.
+                위의 내용을 바탕으로 질문과 예상 답변과 근거를 생성해줘.
+                질문과 예상 답변 그리고 근거를 생성하는 방법을 차례대로 설명해줄께.
+                Date와 Time은 해당 채팅을 입력한 날짜와 시간이야.
+                """;
+
+            String add = """
+                
+                위에 대화 내용을 바탕으로 객관식 문제를 생성해줘.
+                    객관식은 사용자가 어제의 일을 잘 기억하고있나 체크하기위한 객관식이야.
+                    객관식 문항은 총 4가지로 되어있고, 정답은 오직 하나야.
+                    객관식의 문항은 단어로 구성되어야만해.
+                    문제가 너무 쉽지 않도록, 정답과 유사성이 있는 단어로 문항을 구성해줘.
+                    질문은 반드시 제공해준 어제 대화 내용에 관한 질문이여야해.
+                    질문은 반드시 과거에 대한 질문이여야해.
+                    사용자가 라는 단어는 사용하지말고, 질문해줘.
+                    질문을 제공할때 존댓말을 사용해줘.
+                    질문의 형태는 공손한 대화형 질문체를 사용해줘.
+                                    
+                    내가 제공해준 내용을 바탕으로 3개의 객관식 문제를 제공해줘.
+                    문제와 문항, 정답 총 3가지를 제공해줘.
+                    정답의 이유도 제공해줘.
+                    정답의 이유는 제공해준 대화내용에 근거해야해.
+                    추측성 근거는 제시하지말아줘.
+                                    
+                    예시 형태를 제공해줄게.
+                    ex)질문: 질문입니다! 어제 점심에 무었을 드셨나요?@
+                    1. 삼겹살 2. 라면 3. 비빔밥 4. 소고기@
+                    정답 : 4@
+                    이유 : 어제 점심에 라면을 드시러 간다고 하셨습니다.@
+                                    
+                    데이터를 파싱해서 저장할 수 있도록 예시 형태를 잘 유지해줘.
                 """;
             String diaryMessages = diaryService.getYesterdayDiaryData(userId);
 //            String chatMessages = chatService.getChatMessage(userId,
